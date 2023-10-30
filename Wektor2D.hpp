02200668201a1b6fb6960d8628a9dcb53ed8adff
noldo23 <121789446+noldo23@users.noreply.github.com>
2023-10-30 14:20:44 +0100

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,42 @@
+#include <iostream>
+using namespace std;
+
 class Wektor2D
 {
+    public:
+        Wektor2D()
+        {
+            cout << "Wektor domyœlny" << endl;
+            X = 0;
+            Y = 0;
+            cout << "x=" << X << " y=" << Y << endl;
+        }
+        Wektor2D(double XX, double YY)
+        {
+
+            cout << "Wektor parametryczny" << endl;
+            X = XX;
+            Y = YY;
+            cout << "x=" << X << " y=" << Y << endl;
+        }
+        void setX(double X) { X = X; }
+        double getX() { return X; }
+        void setY(double Y) { Y = Y; }
+        double getY() { return Y; }
+
+       // Operator dodawania wektorów
+        Wektor2D operator+(const Wektor2D& other) const
+        {
+            return Wektor2D(X + other.X, Y + other.Y);
+        }
+
+        // Operator iloczynu skalarnego
+
+        double operator*(const Wektor2D& other) const { return X * other.X + Y * other.Y; 
+        }
+    private:
+        double X,Y;
+
     // Tutaj napisz implementacje klasy Wektor2D
 };
+
